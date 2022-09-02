f4e870311d473ebecf55f71e441c77c7f5ebe4c7
Thomas Min <thomas.min@ewoosoft.com>
2022-09-02 15:45:13 +0900

    feat: add websocket example

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,17 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{ErrorEvent, MessageEvent, WebSocket};
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
 use egui_extras::{Size, StripBuilder};
 use qt_simple_crypt::{simple_crypt::SimpleCrypt, *};
 
@@ -120,6 +134,10 @@ impl eframe::App for TemplateApp {
                 .vertical(|mut strip| {
                     strip.cell(|ui| {
                         ui.horizontal(|ui| {
+                            if ui.button("http").clicked() {
+                                println!("http request");
+                                start_websocket();
+                            }
                             if ui.button("Encrypt").clicked() {
                                 let mut crypto = SimpleCrypt::new(
                                     u64::from_str_radix(&self.cypher_key, 16).unwrap(),
@@ -186,4 +204,79 @@ impl eframe::App for TemplateApp {
             });
         }
     }
+}
+
+// NOTE If uncomment below line, it will be executed when wasm module is instanciated.
+// See https://rustwasm.github.io/wasm-bindgen/reference/attributes/on-rust-exports/start.html
+// #[wasm_bindgen(start)]
+pub fn start_websocket() -> Result<(), JsValue> {
+    // Connect to an echo server
+    let ws = WebSocket::new("wss://echo.websocket.events")?;
+    // For small binary messages, like CBOR, Arraybuffer is more efficient than Blob handling
+    ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
+    // create callback
+    let cloned_ws = ws.clone();
+    let onmessage_callback = Closure::<dyn FnMut(_)>::new(move |e: MessageEvent| {
+        // Handle difference Text/Binary,...
+        if let Ok(abuf) = e.data().dyn_into::<js_sys::ArrayBuffer>() {
+            console_log!("message event, received arraybuffer: {:?}", abuf);
+            let array = js_sys::Uint8Array::new(&abuf);
+            let len = array.byte_length() as usize;
+            console_log!("Arraybuffer received {}bytes: {:?}", len, array.to_vec());
+            // here you can for example use Serde Deserialize decode the message
+            // for demo purposes we switch back to Blob-type and send off another binary message
+            cloned_ws.set_binary_type(web_sys::BinaryType::Blob);
+            match cloned_ws.send_with_u8_array(&vec![5, 6, 7, 8]) {
+                Ok(_) => console_log!("binary message successfully sent"),
+                Err(err) => console_log!("error sending message: {:?}", err),
+            }
+        } else if let Ok(blob) = e.data().dyn_into::<web_sys::Blob>() {
+            console_log!("message event, received blob: {:?}", blob);
+            // better alternative to juggling with FileReader is to use https://crates.io/crates/gloo-file
+            let fr = web_sys::FileReader::new().unwrap();
+            let fr_c = fr.clone();
+            // create onLoadEnd callback
+            let onloadend_cb = Closure::<dyn FnMut(_)>::new(move |_e: web_sys::ProgressEvent| {
+                let array = js_sys::Uint8Array::new(&fr_c.result().unwrap());
+                let len = array.byte_length() as usize;
+                console_log!("Blob received {}bytes: {:?}", len, array.to_vec());
+                // here you can for example use the received image/png data
+            });
+            fr.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
+            fr.read_as_array_buffer(&blob).expect("blob not readable");
+            onloadend_cb.forget();
+        } else if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
+            console_log!("message event, received Text: {:?}", txt);
+        } else {
+            console_log!("message event, received Unknown: {:?}", e.data());
+        }
+    });
+    // set message event handler on WebSocket
+    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+    // forget the callback to keep it alive
+    onmessage_callback.forget();
+
+    let onerror_callback = Closure::<dyn FnMut(_)>::new(move |e: ErrorEvent| {
+        console_log!("error event: {:?}", e);
+    });
+    ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
+    onerror_callback.forget();
+
+    let cloned_ws = ws.clone();
+    let onopen_callback = Closure::<dyn FnMut()>::new(move || {
+        console_log!("socket opened");
+        match cloned_ws.send_with_str("ping") {
+            Ok(_) => console_log!("message successfully sent"),
+            Err(err) => console_log!("error sending message: {:?}", err),
+        }
+        // send off binary message
+        match cloned_ws.send_with_u8_array(&vec![0, 1, 2, 3]) {
+            Ok(_) => console_log!("binary message successfully sent"),
+            Err(err) => console_log!("error sending message: {:?}", err),
+        }
+    });
+    ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
+    onopen_callback.forget();
+
+    Ok(())
 }
